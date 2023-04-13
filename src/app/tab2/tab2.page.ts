5b6f9b4d131eab79df508783464f60f75abb58b7
se10 <prashant.singh2@spit.ac.in>
2023-04-13 13:15:35 +0530

    Started working on page 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,52 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+interface AccordionItem {
+  title: string;
+  expanded: boolean;
+}
 
+interface ListItem{
+  name:String,
+  designation:String,
+  email:String,
+  phone:String,
+  domain:String,
+  image:String,
+
+}
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent,CommonModule]
 })
 export class Tab2Page {
 
   constructor() {}
+  items :ListItem[] = [{
+    name:"Harshil Kanakia",
+    designation:"Head Of Department",
+    email:"Harshi@gmail.com",
+    phone:"12893019824901",
+    domain:"MCA",
+    image:"https://mca.spit.ac.in/wp-content/uploads/2022/12/HK.png"
+  },
+  {
+    name:"Nikhita Mangaonkar",
+    designation:"Head Of Department",
+    email:"nikhita@gmail.com",
+    phone:"12893019824901",
+    domain:"MCA",
+    image:"https://mca.spit.ac.in/wp-content/uploads/2022/12/NM.jpg"
+  }
+
+  ];
+
+  toggleItem(item: AccordionItem) {
+    item.expanded = !item.expanded;
+  }
 
 }
